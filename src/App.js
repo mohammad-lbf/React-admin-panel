eaf6b23d3769d172a689d7fcc072b632608f6a38
mohammad_lbf <labbaf2016@gmail.com>
2022-09-01 08:57:27 +0430

    general structuring done , react-router-dom & styled components installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Article from './components/layout/Article';
 
 const App = () => {
   return (
     <>
-
+      <Article />
     </>
   );
 };
---
+++ src/components/layout/Article.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Aside from './Aside';
+import Main from './Main';
+
+const Article = () => {
+    return (
+        <div>
+            <Main />
+            <Aside />
+        </div>
+    );
+};
+
+export default Article;
